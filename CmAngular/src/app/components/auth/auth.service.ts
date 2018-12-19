d092323e6401953adf72a35b42b974868b83d9e9
Niek Flipse <npaflips@avans.nl>
2018-12-19 11:17:45 +0100

    Created AuthService and AuthGuard

--- CmAngular/src/app/components/auth/auth.service.ts
+++ CmAngular/src/app/components/auth/auth.service.ts
@@ -1,7 +1,37 @@
-import { config } from "../../../assets/config";
+import { HttpClient } from "@angular/common/http";
+import { Router } from "@angular/router";
+import { config } from "src/assets/config";
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class AuthService {
 
-    constructor() { }
+    constructor(
+        private http: HttpClient,
+        private router: Router
+    ) { }
 
+    signinUser(email: String, password: String) {
+        this.logout();
+        this.http.post<any>(`${config.apiUrl}/api/auth`, {
+            email,
+            password
+        })
+            .subscribe((response) => {
+                localStorage.setItem("token", response.token);
+                this.router.navigate(['/']);
+            });
+    }
+
+    logout() {
+        localStorage.clear();
+    }
+    
+    isAuthenticated() {
+        return localStorage.getItem('token') != null;
+    }
+    
+    getToken(): String {
+        return 'Bearer ' + localStorage.getItem('token');
+    }
 }
