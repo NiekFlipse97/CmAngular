8a3821f1dc035bb2be12e7bd142741689dda0a4e
ekrej <ekrej@hotmail.nl>
2018-12-19 11:12:31 +0100

    fixed routing for form

--- CmAngular/src/app/app-routing.module.ts
+++ CmAngular/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ControlCheckFormComponent } from './components/control-check-form/control-check-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'controlcheck/add', component: ControlCheckFormComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
