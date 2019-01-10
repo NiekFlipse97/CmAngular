a2a3bd074127e525011033b8bc7b21db11d8de8b
Niek Flipse <npaflips@avans.nl>
2019-01-10 10:47:26 +0100

    Fixed login form

--- CmAngular/src/app/components/auth/signin/signin.component.ts
+++ CmAngular/src/app/components/auth/signin/signin.component.ts
@@ -11,12 +11,12 @@ export class SigninComponent implements OnInit {
 
   loginForm = new FormGroup({
     username: new FormControl('', [
-        Validators.required
+      Validators.required
     ]),
     password: new FormControl('', [
-        Validators.required
+      Validators.required
     ])
-})
+  })
 
   constructor(
     private authService: AuthService
@@ -29,7 +29,9 @@ export class SigninComponent implements OnInit {
     const username = this.loginForm.get('username').value;
     const password = this.loginForm.get('password').value;
 
+    console.log(username, password);
+
     this.authService.signinUser(username, password);
-}
+  }
 
 }
