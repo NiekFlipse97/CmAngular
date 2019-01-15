0f247885b76093ebed6e7450a71547407b020c30
Niek Flipse <npaflips@avans.nl>
2019-01-15 12:35:18 +0100

    Added methods to control-check.service

--- CmAngular/src/app/control-check/control-check.service.ts
+++ CmAngular/src/app/control-check/control-check.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from "@angular/core";
 import { ControlCheck } from "./control-check-model";
+import { HttpClient } from "@angular/common/http";
+import { config } from "src/assets/config";
+import { Observable } from "rxjs";
 
 @Injectable()
 export class ControlCheckService {
-    constructor() {}
+    constructor(
+        private http: HttpClient
+    ) {}
 
     /**
      * Get a single ControlCheck by it's id.
@@ -11,5 +16,13 @@ export class ControlCheckService {
      */
     getControlCheck(id : number) : ControlCheck {
         return null;
+    }
+
+    getChecks(): Observable<ControlCheck[]> {
+        return this.http.get<ControlCheck[]>(`${config.apiUrl}/api/checks`);
+    }
+
+    deleteCheck(check: ControlCheck) {
+        this.http.delete(`${config.apiUrl}/api/checks/${check._id}`)
     }
 }
